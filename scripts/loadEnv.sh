a8e22cbba5f426916d35fd08c935b3b62e6172b0
shaimoria <shay.moria@zooz.com>
2021-03-03 10:55:00 +0200

    fix: better version handling

--- scripts/loadEnv.sh
+++ scripts/loadEnv.sh
@@ -2,7 +2,9 @@ set -e
 [[ $ENV ]] && echo Environment is $ENV
 
 function setCommonEnvVars() {
-    export VERSION=$(git describe --tags --abbrev=0)
+    export TAGS=$(git tag -l --sort=-v:refname)
+    export LATEST_TAG=$(echo $TAGS | head -n1 | cut -d " " -f1)
+    export LATEST_TAG="${LATEST_TAG%-internal}" # remove internal suffix if exists
     if [ -z ${CI_COMMIT_TAG+x} ] && [[ ! $CI_COMMIT_REF_NAME == cicd* ]]; then
         echo "Docker image will set on branches folder in registry"
         export REL_CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE/branches
@@ -10,13 +12,13 @@ function setCommonEnvVars() {
     else
         echo "Docker image will set on cicd folder in registry (which is the release folder)"
         export REL_CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE/cicd
-        export DOCKER_IMAGE=$REL_CI_REGISTRY_IMAGE:$VERSION
+        export DOCKER_IMAGE=$REL_CI_REGISTRY_IMAGE:$LATEST_TAG
     fi    
 }
 
 function reportEnvVars() {
     echo "DOCKER_IMAGE: $DOCKER_IMAGE"
-    echo "VERSION: $VERSION"
+    echo "LATEST_TAG: $LATEST_TAG"
 }
 
 setCommonEnvVars
