143e68dce58691cbc73935f8d3d1fe35a495a7f9
Shai Moria <shyimo88@gmail.com>
2021-07-21 17:53:41 +0300

    fix: fix acl file parsing (#115)
    * fix: fix acl file parsing
    
    * fix: extend acl unit tests
    
    * fix: fix readme typo
    
    * fix: remove unwanted logs
    
    Co-authored-by: shaimoria <shai.moria@zooz.com>

--- controllers/rediscli/acl_test.go
+++ controllers/rediscli/acl_test.go
@@ -10,7 +10,9 @@ import (
 const ACL string = `1) "user admin on #713bfda78870bf9d1b261f565286f85e97ee614efe5f0faf7c34e7ca4f65baca ~* &* +@all"
 2) "user default on nopass sanitize-payload ~* &* +@all"
 3) "user rdcuser on #400f9f96b4a343f4766d29dbe7bee178d7de6e186464d22378214c0232fb38ca &* -@all +replconf +ping +psync"
-4) "user testuser on #13d249f2cb4127b40cfa757866850278793f814ded3c587fe5889e889a7a9f6c ~testkey:* &* -@all +get +set"`
+4) "user testuser on #13d249f2cb4127b40cfa757866850278793f814ded3c587fe5889e889a7a9f6c ~testkey:* &* -@all +get +set"
+5) "user plaintextuser on >12345678 ~* &* +@all"
+6) "user commandsuser on >987654321 +hget +hset +hgetall ~some_prefix"`
 
 var ACLObj RedisACL = RedisACL{
 	Users: []RedisACLUser{
@@ -76,6 +78,35 @@ var ACLObj RedisACL = RedisACL{
 			Commands: RedisACLCommands{
 				Commands:   []string{"get", "set"},
 				RmCommands: []string{"@all"},
+			},
+		},
+		{
+			Name: "plaintextuser",
+			On:   true,
+			Passwords: RedisACLPasswords{
+				Hashes: []string{"ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f"},
+			},
+			Keys: RedisACLKeys{
+				Patterns: []string{"*"},
+			},
+			Channels: RedisACLChannels{
+				Patterns: []string{"*"},
+			},
+			Commands: RedisACLCommands{
+				Commands: []string{"@all"},
+			},
+		},
+		{
+			Name: "commandsuser",
+			On:   true,
+			Passwords: RedisACLPasswords{
+				Hashes: []string{"8a9bcf1e51e812d0af8465a8dbcc9f741064bf0af3b3d08e6b0246437c19f7fb"},
+			},
+			Keys: RedisACLKeys{
+				Patterns: []string{"some_prefix"},
+			},
+			Commands: RedisACLCommands{
+				Commands: []string{"hget", "hset", "hgetall"},
 			},
 		},
 	},
