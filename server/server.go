623154950bd54f475cd6e413e8e687bbedd895e2
Natali Aharoni <Natali.Aharoni@payu.com>
2022-08-01 19:03:59 +0300

    register metric to prometheus

--- server/server.go
+++ server/server.go
@@ -1,17 +1,17 @@
 package server
 
 import (
-	"github.com/labstack/echo-contrib/prometheus"
+	//"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 )
 
 func StartServer() {
 	echoMain := echo.New()
-	prom := prometheus.NewPrometheus("echo", nil)
+	//prom := prometheus.NewPrometheus("echo", nil)
 
 	// Routes
 	register(echoMain)
-	prom.Use(echoMain)
+	//prom.Use(echoMain)
 
 	// Start server
 	go echoMain.Logger.Fatal(echoMain.Start("0.0.0.0:8080"))
